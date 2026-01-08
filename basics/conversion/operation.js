5f7cddca99791d0f72aa4780f6cf9201ef111436
JatinJawa <jatin10310@gmail.com>
2026-01-08 19:20:28 +0000

    done

---
+++ basics/conversion/operation.js
@@ -0,0 +1,17 @@
+let score="56"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber = Number (score)
+console.log(typeof valueinnumber);
+
+// "33"=>33
+// "56abc"=>Nan
+
+let loggedin=1
+let booleanloggedin=Boolean(loggedin)
+console.log(booleanloggedin);
+//1=> true /0=>false
+//""=> false
+//"jatin"=> false
