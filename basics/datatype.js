cbbc786bed017f2cd883add5f2c911c64b675b92
JatinJawa <jatin10310@gmail.com>
2026-01-08 18:57:29 +0000

    done

---
+++ basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("3+3") we are using node js not browser 
+let name ="jatin "
+let age=21
+let isloggedin=false
+
+//number==> 2 to the power 53
+// bigINT
+//string==>""
+//boolean=> treue/false
+//null=> standalone value 
+// undefine => when value is not given 
+//symbol=> use for unique 
+
+
+//object 
+
+console.log(typeof age );
