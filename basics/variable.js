f80487103b6055e9624a7aa84d0b8b78b9930594
JatinJawa <jatin10310@gmail.com>
2026-01-08 18:42:39 +0000

    container done

---
+++ basics/variable.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountemail="fdsfokeg.com"
+var accountpassword="2345"
+accountcity="qwe"
+let accountstate
+//accountId=2// not allowed 
+accountemail="zxcvv.com"
+accountpassword="123"
+
+console.log(accountId);
+/*
+we do not use var
+as in do not work in block scope and functional scope 
+for example if(){there brackets are scope }
+*/
+console.table([accountId,accountemail,accountpassword,accountstate])
