94229233650d9fe754caca01cb337c24f81d309f
JatinJawa <jatin10310@gmail.com>
2026-01-13 16:42:22 +0000

    done

---
+++ basics/conversion/stringoperation.js
@@ -0,0 +1,15 @@
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+let str1="wtf"
+let str2="qwe"
+let str3=str1+str2
+//console.log(str3);
+
+
+//console.log(+true); this is write but 
+//console.log(true+);this is not it show syntax error
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
